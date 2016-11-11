d86f4986d043802c599c5c2eb0257f19612a7944
Nam Nguyen <nam.ngvan@gmail.com>
2016-11-11 08:57:05 +0700

    prime commit

---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+namespace Phpfox\Widget;
+
+return [
+    'aliases'   => [
+        'widget' => WidgetManager::class,
+    ],
+    'factories' => [
+        WidgetManager::class => null,
+    ],
+];
